4f1658d6161abdcf06b96772dfa6d213d150973a
deepwwwave <deepwwwave@gmail.com>
2020-12-14 14:30:51 +0100

    Ajout nodemode

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const env = require('dotenv');
 const app = express();
+const bodyParser = require('body-parser');
+
+// Evironnement variable or you can say constante
+env.config();
+
+// app.use(bodyParser());
+
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Hello frorm Server'
+    });
+});
+
+app.post('/data', (req, res, next) => {
+    res.status(200).json({
+        message: req.body
+    });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`server is runnig on port ${process.env.PORT}`);
-})
+});
