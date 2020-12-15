b108a7c1c44f072ad9cbf762c184a599c66f7afd
deepwwwave <deepwwwave@gmail.com>
2020-12-15 11:55:26 +0100

    ajout configuration mongoose/mongoDB en cours

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -2,12 +2,22 @@ const express = require('express')
 const env = require('dotenv');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // Evironnement variable or you can say constante
 env.config();
 
 // parse application/x-www-form-urlencoded
 // app.use(bodyParser.urlencoded({ extended: false }))
+
+//MongoDB connexion
+// mongodb+srv:root:<password>@cluster0.t2zq5.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose.connect(
+    'mongodb://localhost:27017/myapp', 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 
 // parse application/json
 app.use(bodyParser.json())
@@ -16,6 +26,8 @@ app.use(bodyParser.json())
 app.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Hello frorm Server'
+    }).then(() => {
+        console.log('Database connected');
     });
 });
 
