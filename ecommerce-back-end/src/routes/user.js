b209952c2764298be9d73739c5414a425371de9b
deepwwwave <deepwwwave@gmail.com>
2020-12-17 20:36:04 +0100

    user controllers/models/routes OK , data ok on postman

--- ecommerce-back-end/src/routes/user.js
+++ ecommerce-back-end/src/routes/user.js
@@ -1,56 +1,14 @@
+
+
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user')
+const { signup } = require('../controller/user');
 
 
 router.post('/signin', (req, res) => {
 
-    // User.findOne({ email:req.body.email })
-    // .exec((error, user) => {
-    //     if(error || !user) return res.status(400).json({
-    //         message: "User not found"
-    //     });
-
 });
 
-router.post('/signup', (req, res) => {
-
-
-    User.findOne({ email: req.body.email })
-    .exec((error, user) => {
-        if(user) return res.status(400).json({
-            message: "User already registered"
-        });
-        
-        const {
-            firstName,
-            lastName,
-            email,
-            password
-        } = req.body;
-        const _user = new User({
-            firstName,
-            lastName,
-            email,
-            password,
-            username: Math.random().toString()
-         });
-        
-         _user.save((error, date) => {
-             if(error){
-                 return res.status(400).json({
-                     message: 'Something went wrong'
-                 });
-             }
-
-             if(data){
-                 return res.status(201).json({
-                     user:data
-                 })
-             }
-         });
-    });
-
-});
+router.post('/signup', signup );
 
 module.exports = router;
